9d0f98061fb2c0b9a3dfd5f040dfbaf3d5706289
root <root@DESKTOP-T112RKM>
2023-08-12 09:47:33 +0700

    state & setState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('React')
+  const [address, setAddress] = useState('')
+
+  const handleEventClick = (event) => {
+    setName(address)
+  }
+  const handleOnChangeInput = (e) => {
+    setAddress(e.target.value)
+  }
+
   return (
     <div className="App">
+       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>HELLO WORLD WITH {name}</h2>
+        <input type='text' value={address} onChange={e => handleOnChangeInput(e)}/>
+        <button type='button' onClick={(event) => handleEventClick(event)}>Click me!</button>
       </header>
     </div>
   );
